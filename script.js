6af77090e54a2c437f65f9ae6c1ec107fbd26807
DarkyWarky <121173192+DarkyWarky@users.noreply.github.com>
2023-09-01 11:21:04 +0530

    done

---
+++ script.js
@@ -0,0 +1,46 @@
+const robotParts = [
+    {
+      name: "Microcontroller Board",
+      category: "Electronic",
+      price: 29.99,
+      description: "Powerful microcontroller for controlling robot functions.",
+      photo: "https://example.com/microcontroller.jpg"
+    },
+    {
+      name: "Motor Kit",
+      category: "Mechanical",
+      price: 39.95,
+      description: "High-torque motors for robot movement and mobility.",
+      photo: "https://example.com/motor-kit.jpg"
+    },
+    {
+      name: "Ultrasonic Sensor",
+      category: "Electronic",
+      price: 12.50,
+      description: "Detects obstacles and measures distances using sound waves.",
+      photo: "https://example.com/ultrasonic-sensor.jpg"
+    },
+    {
+      name: "Camera Module",
+      category: "Electronic",
+      price: 45.99,
+      description: "Captures images and videos for robot vision.",
+      photo: "https://example.com/camera-module.jpg"
+    },
+    {
+      name: "Robotics Starter Kit",
+      category: "Robo-Kits",
+      price: 89.99,
+      description: "Beginner-friendly kit with essential components for building robots.",
+      photo: "https://example.com/robotics-starter-kit.jpg"
+    },
+    {
+      name: "Servo Motor",
+      category: "Motors",
+      price: 8.75,
+      description: "Precise motor for controlling robot arm and joint movements.",
+      photo: "https://example.com/servo-motor.jpg"
+    }
+  ];
+  
+  
